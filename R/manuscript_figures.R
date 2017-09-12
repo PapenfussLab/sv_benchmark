5ca242a2460329da80dc015de8d307f6ceee8239
l-d-s <l@sdf.org>
2017-09-12 17:02:46 +1000

    Correct ids for chm1 & 13

--- R/manuscript_figures.R
+++ R/manuscript_figures.R
@@ -48,13 +48,17 @@ longreadbedpedir <- NULL #TODO use Eichler PacBio reads directly
 datadir <- "../data.chm13"
 sample_name <- "chm13"
 ids <- c(
-	"16c58fbcc5633564b10ebe8f78d87883",
-	"40c68f29b6d7cb2358f31a7073250406",
-	"43a13d07730deb934e9fc01e3b3cd26f",
-	"8dcad8fe04f4ebc0ad3254ab4420cdc8",
-	"acd889cc16741fb0fba62faa4f7005f3",
-	"b1112f1c3cbd28c464f58fc5c5c02f9b",
-	"9d134f160ac68c0445002fbb78db4a5e")
+    "1bf3f864802712ffbe88f5bf29c7035a", # breakdancer/1.4.5
+    "3b4917f2b3fc05710961bd98fe795863", # hydra/master20160129
+    "48321dad78b7fdd801f6778c438799b3", # delly/0.7.6
+    "4c7e0c2ae616fe15db24b2caf3786b93", # socrates/1.13.1
+    "5ececd73193ffa02446ec5e2ea269fab", # pindel/0.2.5b6
+    "6ab531d72c9b112e4a798e4285205cf3", # lumpy/0.2.11
+    "907ff8ccd8657b8554ec31feb09f461b", # gridss/1.4.1
+    "a6034953f1524610656f002b74faa1c4", # crest
+    "b264ac30faf22d80ef51d8c4a25b1275"  # manta/1.1.1
+    # "c639766990fcfbca2eb45f7806362fe6" # bcftools/1.3.1
+	)
 truth_id <- "00000000000000000000000000000013"
 truth_name <- "Huddleston et al"
 grtransformName <- "None"
@@ -64,13 +68,16 @@ generate_figures(datadir, sample_name, ids, truth_id, truth_name, grtransformNam
 datadir <- "../data.chm1"
 sample_name <- "chm1"
 ids <- c(
-	"16c58fbcc5633564b10ebe8f78d87883",
-	"40c68f29b6d7cb2358f31a7073250406",
-	"43a13d07730deb934e9fc01e3b3cd26f",
-	"8dcad8fe04f4ebc0ad3254ab4420cdc8",
-	"acd889cc16741fb0fba62faa4f7005f3",
-	"b1112f1c3cbd28c464f58fc5c5c02f9b",
-	"9d134f160ac68c0445002fbb78db4a5e")
+    "16c58fbcc5633564b10ebe8f78d87883", # manta/1.1.1
+    "43a13d07730deb934e9fc01e3b3cd26f", # hydra/master20160129
+    "74a311b09582df074b2bd839b06f8775", # pindel/0.2.5b6
+    "8dcad8fe04f4ebc0ad3254ab4420cdc8", # breakdancer/1.4.5
+    # "94bb6deef9f1bf1f9027a47e8488ae4f", # bcftools/1.3.1
+    "9d134f160ac68c0445002fbb78db4a5e", # delly/0.7.6
+    "acd889cc16741fb0fba62faa4f7005f3", # lumpy/0.2.11
+    "b1112f1c3cbd28c464f58fc5c5c02f9b", # gridss/1.4.1
+    "b142875fd36e402e4aff5f9e31f257f7")  # socrates/1.13.1
+
 truth_id <- "00000000000000000000000000000001"
 truth_name <- "Huddleston et al"
 grtransformName <- "None"
